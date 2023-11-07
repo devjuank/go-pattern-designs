16ea70332c592a83398ca1bde6780cc2c56f4d80
Juan Carlos Ojeda Gomez <juanki.ijcog@gmail.com>
2023-11-07 12:35:38 -0300

    using RWMutex for read and write locks

--- sync/main.go
+++ sync/main.go
@@ -7,7 +7,7 @@ import (
 
 var balance = 100
 
-func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.RWMutex) {
 	defer wg.Done()
 
 	lock.Lock()
@@ -15,17 +15,20 @@ func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
 	lock.Unlock()
 }
 
-func Balance() int {
-	return balance
+func Balance(lock *sync.RWMutex) int {
+	lock.RLock()
+	b := balance
+	lock.RUnlock()
+	return b
 }
 func main() {
 	var wg sync.WaitGroup
-	var lock sync.Mutex
+	var lock sync.RWMutex
 
 	for i := 1; i <= 5; i++ {
 		wg.Add(1)
 		go Deposit(i*100, &wg, &lock)
 	}
 	wg.Wait()
-	fmt.Println(Balance())
+	fmt.Println(Balance(&lock))
 }
